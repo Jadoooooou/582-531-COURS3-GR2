15ed56eb4d00761a1c40e70c1ca513f0d6a9580d
Jadoooooou <2366906@cmontmorency.qc.ca>
2025-09-09 17:44:21 -0400

    couleur_pixel

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,25 @@
 #include <Arduino.h>
+#include <FastLED.h>
 #include <MicroOscSlip.h>
 MicroOscSlip<128> monOsc(&Serial);
 
 #define MA_BROCHE_ANGLE 32
 #define MA_BROCHE_BOUTON 39
+CRGB monPixel;
 
 void setup() {
   pinMode( MA_BROCHE_BOUTON , INPUT );
 
   Serial.begin(115200);
+
+  FastLED.addLeds<WS2812, 27, GRB>(&monPixel, 1);
 }
 
 void loop() {
-  int maLectureBouton = digitalRead( MA_BROCHE_BOUTON );
-  Serial.print(maLectureBouton);
-  Serial.println();
-  delay(100);
+  //int maLectureBouton = digitalRead( MA_BROCHE_BOUTON );
+  //Serial.print(maLectureBouton);
+  //Serial.println();
+  //delay(100);
 
   
   // oscslip_pd
@@ -27,4 +31,7 @@ void loop() {
   monOsc.sendInt("/angle", maLectureAnalogique);
   delay(100);
 
+  // couleur_pixel
+  monPixel = CRGB(0, millis() % 25, 0);
+  FastLED.show();
 }
